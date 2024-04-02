3d459ec6b4217bd2e1798b541816559634699de6
Hassieb <hassieb.n.h@gmail.com>
2024-04-02 16:34:33 -0700

    Working on sign up page and data base.

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/lorde/Desktop/firstFullStackProject/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+app.app_context().push()
 
 class userInfo(db.Model):
-        userName = db.Column(db.String(), primary_key = True)
-        password = db.Column(db.String(), nullable = False)
+        id = db.Column(db.Integer, primary_key = True)
+        userName = db.Column(db.String(200))
+        password = db.Column(db.String(100), nullable = False)
         dateCreated = db.Column(db.DateTime ,default=datetime.utcnow)
 
         #returns task and userName as a string 
         def __repr__(self):
             return "<Task %r>" % self.userName
-@app.route("/")
+        
+
+
+@app.route("/", methods=["POST" ,"GET"])
 def home():
-    return render_template("login.html")
+    if request.method == "POST":
+        pass
+        return "hello world"
+    else:
+        return render_template("login.html")
+
+         
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-#todo: Get my test.db file working 
-import os
-print('test.db exists:', os.path.exists('test.db'))
